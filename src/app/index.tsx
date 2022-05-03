882db147e467adc548bb1c09d217e47403091e9a
Sviridov Konstantin <kostya.sviridov.01@mail.ru>
2022-05-04 01:18:56 +0700

    feat: create react-app

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./index.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Свиридов Константин 8К93</h1>
+            <h1>Садиков Максим 8К93</h1>
+        </div>
+    );
+}
+
+export default App;
