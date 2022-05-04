6ce92f7d37b563456ca28d747e1136e5bfb61d60
Sviridov Konstantin <kostya.sviridov.01@mail.ru>
2022-05-05 02:57:54 +0700

    feat: login and password validation

--- src/entities/siginInForm/ui/index.tsx
+++ src/entities/siginInForm/ui/index.tsx
@@ -15,7 +15,10 @@ const SignInForm: React.FC = () => {
         value: "",
         errors: []
     });
-    const [password, setPassword] = useState("");
+    const [password, setPassword] = useState<ILogin>({
+        value: "",
+        errors: []
+    });
     const [showPassword, setShowPassword] = useState(false);
 
     const changeLoginState = (value: string) => {
@@ -28,7 +31,10 @@ const SignInForm: React.FC = () => {
         });
     }
     const changePasswordState = (value: string) => {
-        setPassword(value);
+        const error = [];
+        if (requiredField(value) != true) error.push(requiredField(value))
+        else if (maxLength(value, 30) != true) error.push(maxLength(value, 30));
+        setPassword({value: value, errors: error});
     }
     const changeShowPasswordState = () => {
         setShowPassword(!showPassword);
@@ -39,44 +45,61 @@ const SignInForm: React.FC = () => {
             <div className={styles.signInBlock}>
                 <h1 className={styles.formHeader}>Войти</h1>
                 <div>
-                    <Box fontFamily={"Gilroy"} sx={{display: 'flex', alignItems: 'flex-end', width: 1}}>
+                    <FormControl sx={{width: 1}} variant="outlined">
                         {login.errors.length === 0 ?
                             <TextField id="input-with-sx" value={login.value}
                                        onChange={event => changeLoginState(event.target.value)}
                                        label="Логин"
-                                       variant="standard" sx={{width: 1}}/> :
+                                       variant="standard"
+                                       sx={{width: 1}}/> :
                             <TextField
                                 error
                                 id="standard-error-helper-text"
                                 label="Логин"
                                 sx={{width: 1}}
-                                defaultValue={login.value}
+                                value={login.value}
                                 onChange={event => changeLoginState(event.target.value)}
                                 helperText={login.errors[0]}
                                 variant="standard"
                             />}
-
-                    </Box>
+                    </FormControl>
 
                 </div>
                 <div className={styles.password}>
                     <FormControl sx={{width: 1}} variant="outlined">
-                        <InputLabel variant={"standard"} htmlFor="standard-adornment-password">Пароль</InputLabel>
-                        <Input
-                            id="standard-adornment-password"
-                            type={showPassword ? 'text' : 'password'}
-                            value={password}
-                            onChange={event => changePasswordState(event.target.value)}
-                            endAdornment={
-                                <InputAdornment position="end">
+                        {password.errors.length === 0 ?
+                            <><InputLabel variant={"standard"} htmlFor="standard-adornment-password">Пароль</InputLabel><Input
+                                id="standard-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                value={password.value}
+                                onChange={event => changePasswordState(event.target.value)}
+                                endAdornment={<InputAdornment position="end">
                                     <IconButton
                                         aria-label="toggle password visibility"
                                         onClick={changeShowPasswordState}>
                                         {showPassword ? <VisibilityOff/> : <Visibility/>}
                                     </IconButton>
-                                </InputAdornment>
-                            }
-                        />
+                                </InputAdornment>}/></> :
+                            <>
+                                <TextField
+                                    error
+                                    id="standard-error-helper-text"
+                                    helperText={password.errors[0]}
+                                    variant="standard"
+                                    type={showPassword ? 'text' : 'password'}
+                                    label={"Пароль"}
+                                    value={password.value}
+                                    onChange={event => changePasswordState(event.target.value)}
+                                    InputProps={{
+                                        endAdornment: (<InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={changeShowPasswordState}>
+                                                {showPassword ? <VisibilityOff/> : <Visibility/>}
+                                            </IconButton>
+                                        </InputAdornment>)
+                                    }}
+                                /></>}
                     </FormControl>
                 </div>
                 <Button className={styles.button} variant="contained">Войти</Button>
