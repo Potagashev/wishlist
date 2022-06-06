c6cb8d5430d61a2e98dacec5ccfd783d8dd5a15e
IlyaKuz26 <95118488+IlyaKuz26@users.noreply.github.com>
2022-06-07 05:17:54 +0700

    add files for rec_system

---
+++ train_item2item.py
@@ -0,0 +1,41 @@
+
+import json
+import os
+import pickle
+
+import implicit
+import numpy as np
+from scipy import sparse as sp
+from tqdm import tqdm
+
+import config as cfg
+
+if __name__ == "__main__":
+
+    product_encoder = ProductEncoder(cfg.PRODUCT_CSV_PATH)
+
+    rows = []
+    for i in range(15):
+        for js in tqdm((json.loads(s) for s in open(get_shard_path(i)))):
+            rows.append(make_coo_row(js["transaction_history"], product_encoder))
+    train_mat = sp.vstack(rows)
+
+    model = implicit.nearest_neighbours.CosineRecommender(K=1)
+    model.fit(train_mat.T)
+    out_dir = "../tmp/implicit_cosine1/"
+    os.makedirs(out_dir, exist_ok=True)
+    print("Dump model to " + out_dir)
+    pickle.dump(model, open(out_dir + "/model.pkl", "wb"))
+
+    print("Estimate quiality...")
+    scores = []
+    for js in tqdm((json.loads(s) for s in open(get_shard_path(15)))):
+        row = make_coo_row(js["transaction_history"], product_encoder).tocsr()
+        raw_recs = model.recommend(
+            userid=0, user_items=row, N=30, filter_already_liked_items=False, recalculate_user=True
+        )
+        recommended_items = product_encoder.toPid([idx for (idx, score) in raw_recs])
+        gt_items = js["target"][0]["product_ids"]
+        ap = normalized_average_precision(gt_items, recommended_items)
+        scores.append(ap)
+    print("\tmap: {}".format(np.mean(scores)))
